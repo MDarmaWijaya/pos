4fe067a75de33d8f4de7dcd73f026013c5d46201
MDarmaWijaya <wijayamdarma0@gmail.com>
2025-02-21 21:52:39 +0700

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>{{ __('common.Version') }}</b> 3.0.3
+    </div>
+    <strong>{{ __('common.Copyright') }} &copy; 2025 Doa Ibu.</strong> {{ __('common.all_reserved') }}
+  </footer>
